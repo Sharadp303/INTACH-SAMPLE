6c8b0260c5dd3d1c6a9597a35d73ef9602642843
Sharad patel <sharad.patels03@gmail.com>
2023-08-18 20:06:10 +0530

    css

--- src/Components/SideContent/sidecontent.jsx
+++ src/Components/SideContent/sidecontent.jsx
@@ -8,7 +8,7 @@ const SideContent=()=>{
           <p><img src="./Images/new-animated.png" alt="new" />Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aperiam assum tenetur natus, aliquid mollitia quasi molestiae facilis corrupti. Aspernatur quibusdam quod optio voluptatibus adipisci!</p>
           <h3>Featured Video</h3>
          
-          <iframe src="https://www.youtube.com/embed/S4wKoTXyl3k" title="Hello1" width={200} height={150}></iframe>
+          <iframe src="https://www.youtube.com/embed/S4wKoTXyl3k" title="Hello1" width={250} height={150}></iframe>
           <p><img src="./Images/new-animated.png" alt="new" />Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aperiam assum tenetur natus, aliquid mollitia quasi molestiae facilis corrupti. Aspernatur quibusdam quod optio voluptatibus adipisci!</p>
           
           </div>
--- src/Components/Slider/slider.jsx
+++ src/Components/Slider/slider.jsx
@@ -5,32 +5,32 @@ const data = [
   {
     src: "./Images/Banner/baneer10.jpg",
     alt: "Image 1 for carousel",
-    desc:"MOU signed by Maj. Gen. L.K. Gupta, Chairman INTACH with H.E. Dr. Sum Map, Dir. Gen. APSARA (national authority for the protection and management of Angkor &amp; the region of Seim Reap), Govt.of Comodia on 22nd June 2016 at Siem Reap, Cambodia for the period of three years "
+    desc: "MOU signed by Maj. Gen. L.K. Gupta, Chairman INTACH with H.E. Dr. Sum Map, Dir. Gen. APSARA (national authority for the protection and management of Angkor &amp; the region of Seim Reap), Govt.of Comodia on 22nd June 2016 at Siem Reap, Cambodia for the period of three years ",
   },
   {
     src: "./Images/Banner/baneer7.jpg",
     alt: "Image 2 for carousel",
-    desc:"Manimahesh Lake with Kailash Peak in the Background [Himachal Pradesh]"
+    desc: "Manimahesh Lake with Kailash Peak in the Background [Himachal Pradesh]",
   },
   {
     src: "./Images/Banner/baners3.jpg",
     alt: "Image 3 for carousel",
-    desc:"Sampurnanand Sanskrit University, Varanasi"
+    desc: "Sampurnanand Sanskrit University, Varanasi",
   },
   {
     src: "./Images/Banner/baneer15.jpg",
     alt: "Image 4 for carousel",
-    desc:"Traditional weaving, Assam, ICH Documentation"
+    desc: "Traditional weaving, Assam, ICH Documentation",
   },
   {
     src: "./Images/Banner/baners2.jpg",
     alt: "Image 4 for carousel",
-    desc:"Pani Kotha, Diu"
+    desc: "Pani Kotha, Diu",
   },
   {
     src: "./Images/Banner/baneer9.jpg",
     alt: "Image 5 for carousel",
-    desc:"Tillage of land using bullocks - a traditional farming practice [ Rani Bagh, Rajnagar, Khajuraho(M.P)]"
+    desc: "Tillage of land using bullocks - a traditional farming practice [ Rani Bagh, Rajnagar, Khajuraho(M.P)]",
   },
 ];
 
@@ -39,7 +39,7 @@ const Slider = () => {
 
   const nextSlide = useCallback(() => {
     setSlide(slide === data.length - 1 ? 0 : slide + 1);
-  },[slide])
+  }, [slide]);
 
   const prevSlide = () => {
     setSlide(slide === 0 ? data.length - 1 : slide - 1);
@@ -54,6 +54,39 @@ const Slider = () => {
     <>
       <div className="carousel">
         <div className="carousel-container">
+          {data.map((item, idx) => {
+            return (
+              <>
+              
+               <img
+                  src={item.src}
+                  key={idx}
+                  className={slide === idx ? "slide" : "slide-hidden"}
+                  alt={item.alt}
+                />
+                <div className="description">
+                  <span className={slide === idx ? "desc" : "desc-hidden"}>
+                    {item.desc}
+                  </span>
+                </div>
+               
+              </>
+            );
+          })}
+
+          <span className="indicators">
+            {data.map((_, idx) => {
+              return (
+                <button
+                  key={idx}
+                  className={
+                    slide === idx ? "indicator" : "indicator indicator-inactive"
+                  }
+                  onClick={() => setSlide(idx)}
+                ></button>
+              );
+            })}
+          </span>
           <img
             src="./Images/left-arrow.png"
             alt="arrow-icon"
@@ -61,24 +94,6 @@ const Slider = () => {
             className="arrow arrow-left"
           />
 
-          {data.map((item, idx) => {
-            return (
-              <>
-              <img
-                src={item.src}
-                key={idx}
-                className={slide === idx ? "slide" : "slide-hidden"}
-                alt={item.alt}
-                />
-                <div className="description">
-                <span className={slide === idx ? "slide" : "slide-hidden"}>{item.desc}</span>
-                </div>
-                
-              
-              </>
-            );
-          })}
-          
           <img
             src="./Images/right-arrow.png"
             alt="arrow-icon"
@@ -86,20 +101,6 @@ const Slider = () => {
             className="arrow arrow-right"
           ></img>
         </div>
-
-        <span className="indicators">
-          {data.map((_, idx) => {
-            return (
-              <button
-                key={idx}
-                className={
-                  slide === idx ? "indicator" : "indicator indicator-inactive"
-                }
-                onClick={() => setSlide(idx)}
-              ></button>
-            );
-          })}
-        </span>
       </div>
     </>
   );
